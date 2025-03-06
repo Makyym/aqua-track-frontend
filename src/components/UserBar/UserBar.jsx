abf91442d9ff5765a47b7bd2f69e914753fa8a0f
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2025-03-07 00:52:29 +0100

    VM#1

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,12 +1,36 @@
-import UserBarPopover from "../UserBarPopover/UserBarPopover.jsx"
-
+import { useState } from 'react';
+import UserBarPopover from '../UserBarPopover/UserBarPopover.jsx';
+import css from './UserBar.module.css';
 const UserBar = () => {
-    return (
-        <div>
-            UserBar
-            <UserBarPopover />
-        </div>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default UserBar
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLogout = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className={css.relativeHeader}>
+      <h1 className={css.hello}>
+        Hello <span className={css.userName}>,UserName</span>
+      </h1>
+
+      <button type="button" onClick={toggleMenu} className={css.btnHeader}>
+        Nadia
+        <img
+          src="https://i.pravatar.cc/40"
+          alt="User Avatar"
+          className={css.avatar}
+        />
+        <span className={css.btnIcon}>▼</span>
+      </button>
+
+      {isOpen && <UserBarPopover />}
+    </div>
+  );
+};
+
+export default UserBar;
