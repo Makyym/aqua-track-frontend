003788c4a7396ce88ec29b0af4c386af42386088
Makyym <168359394+Makyym@users.noreply.github.com>
2025-03-11 14:31:22 +0200

    waterSlice

--- src/components/BaseModal/BaseModal.jsx
+++ src/components/BaseModal/BaseModal.jsx
@@ -14,8 +14,8 @@ const customStyles = {
     borderRadius: '15px',
     border: 'none',
     outline: 'none',
-    maxHeight: '90vh',
-    overflow: 'scroll'
+    // maxHeight: '90vh',
+    // 'overflow-y': 'scroll',
   },
   overlay: {
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
