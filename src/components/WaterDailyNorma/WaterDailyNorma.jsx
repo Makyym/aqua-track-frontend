15813c8b206224e9e988e90d93664a0a55684d84
Makyym <168359394+Makyym@users.noreply.github.com>
2025-03-14 17:02:10 +0200

    fixed daily norm

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,9 +1,17 @@
+import { useSelector } from "react-redux"
 import s from "./WaterDailyNorma.module.css"
+import { selectUser } from "../../redux/auth/selectors.js"
 
 const WaterDailyNorma = () => {
+    const user = useSelector(selectUser);
+    function convertMlToLiters(ml) {
+        const liters = ml / 1000;
+        return Math.round(liters * 100) / 100;
+    };
+    const waterNorm = convertMlToLiters(user.dailyNorm);
     return (
         <div className={s.div}>
-            <span>1.5 L</span>
+            <span>{waterNorm} L</span>
             <p>My daily norma</p>
         </div>
     )
