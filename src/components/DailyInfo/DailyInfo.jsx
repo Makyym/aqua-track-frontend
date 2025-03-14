18ccafc57561c47850d8f86b094ea5ce2cd89aa1
Makyym <168359394+Makyym@users.noreply.github.com>
2025-03-14 16:09:33 +0200

    done

--- src/components/DailyInfo/DailyInfo.jsx
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -1,12 +1,16 @@
 import AddWaterBtn from "../AddWaterBtn/AddWaterBtn.jsx"
 import ChooseDate from "../ChooseDate/ChooseDate.jsx"
 import WaterList from "../WaterList/WaterList.jsx"
+import s from "./DailyInfo.module.css"
 
 const DailyInfo = () => {
+    const daily = true;
     return (
         <div>
-            <ChooseDate />
-            <AddWaterBtn />
+            <div className={s.div}>
+                <ChooseDate />
+                <AddWaterBtn daily={daily}/>
+            </div>
             <WaterList />
         </div>
     )
--- src/redux/water/selectors.js
+++ src/redux/water/selectors.js
@@ -1,4 +1,4 @@
-export const selectAqua = state => state.water?.items || [];
+export const selectWaterDayArray = state => state.water.waterDay;
 export const selectIsError = state => state.water?.isError || null;
 export const selectIsLoading = state => state.water?.isLoading || false;
 export const selectWaterMonth = state => state.water.waterMonth;
