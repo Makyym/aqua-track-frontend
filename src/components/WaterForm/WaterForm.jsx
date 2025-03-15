75b60336961997fc70791cb7ce0ce071b0076145
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2025-03-15 12:36:29 +0100

    styles

--- src/components/WaterForm/WaterForm.jsx
+++ src/components/WaterForm/WaterForm.jsx
@@ -42,13 +42,13 @@ const WaterForm = () => {
     },
   });
 
-  // const onSubmit = async data => {
-  //   try {
-  //     dispatch(addWater(data));
-  //   } catch (error) {
-  //     alert('Failed to add water');
-  //   }
-  // };
+  const onSubmit = async data => {
+    try {
+      dispatch(addWater(data));
+    } catch (error) {
+      alert('Failed to add water');
+    }
+  };
 
   const timeValue = watch('time') || currentTime;
   const waterValue = watch('value') || 50;
@@ -68,7 +68,10 @@ const WaterForm = () => {
             type="button"
             onClick={handleMinusClick}
           >
-            -
+            Icon Family Classic Select an Icon Style
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
+            </svg>
           </button>
           <span className={css.waterAmount}>{waterValue}</span>
 
--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -1,15 +1,50 @@
+import { useSelector } from 'react-redux';
 import WaterForm from '../WaterForm/WaterForm.jsx';
+import { selectIsLoading } from '../../redux/auth/selectors.js';
 
-const WaterModal = ({ addWaterEntry, editWaterEntry }) => {
+import css from './WaterModal.module.css';
+const WaterModal = ({ addWaterEntry, editWaterEntry, onClose }) => {
+  const isLoading = useSelector(selectIsLoading);
+  const handleClose = () => {
+    onClose();
+  };
   return (
-    <div>
-      <h2 className="title">Add water</h2>
-      <button type="button" onClick={close}>
-        svg
+    <div className={css.modal}>
+      <button type="button" onClick={handleClose} className={css.closeBtn}>
+        <svg
+          className={css.svg}
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M18 6L6 18"
+            stroke="#2F2F2F"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M6 6L18 18"
+            stroke="#2F2F2F"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />{' '}
+        </svg>
       </button>
-      <h2 className="title">Edit the entered amount of water</h2>
-      <h3>Choose value:</h3>
-      <h3>Correct entered data:</h3>
+      {addWaterEntry ? (
+        <h2 className={css.title}>Add water</h2>
+      ) : (
+        <h2 className={css.title}>Edit the entered amount of water</h2>
+      )}
+      {addWaterEntry ? (
+        <h3 className={css.subtitle}>Choose value:</h3>
+      ) : (
+        <h3 className={css.subtitle}>Correct entered data:</h3>
+      )}
       <WaterForm />
     </div>
   );
