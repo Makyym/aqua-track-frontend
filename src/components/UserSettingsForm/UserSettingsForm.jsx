248a95e701b62146ffb0319310a3f28fce2136fe
Makyym <168359394+Makyym@users.noreply.github.com>
2025-03-15 20:59:09 +0200

    some problems

--- src/components/UserSettingsForm/UserSettingsForm.jsx
+++ src/components/UserSettingsForm/UserSettingsForm.jsx
@@ -3,7 +3,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import css from './UserSettingsForm.module.css';
 import clsx from 'clsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import newSprite from '../../assets/newSprite.svg';
 import { selectUser } from '../../redux/auth/selectors';
@@ -41,10 +41,9 @@ const calculateWaterNorm = ({ weight, time, gender }) => {
 };
 
 const UserSettingsForm = ({ onSuccessSubmit }) => {
-  // const [photo, setPhoto] = useState('https://i.pravatar.cc/80');
   const user = useSelector(selectUser);
-  const { name, email, gender, dailySportTime, weight, dailyNorm, avatarUrl } =
-    user;
+  const { name, email, gender, dailySportTime, weight, dailyNorm, avatarUrl } = user;
+  const [preview, setPreview] = useState(avatarUrl);
   const dispatch = useDispatch();
   const {
     register,
@@ -98,11 +97,26 @@ const UserSettingsForm = ({ onSuccessSubmit }) => {
     gender,
   });
   const watchedFiles = watch('avatarUrl');
+  useEffect(() => {
+    if (
+      watchedFiles &&
+      ((watchedFiles instanceof FileList && watchedFiles.length > 0) ||
+        (Array.isArray(watchedFiles) && watchedFiles.length > 0))
+    ) {
+      const file = watchedFiles[0];
+      if (file instanceof File) {
+        const previewURL = URL.createObjectURL(file);
+        setPreview(previewURL);
+        return () => URL.revokeObjectURL(previewURL);
+      }
+    }
+  }, [watchedFiles]);
+  
   return (
     <div className={css.formDiv}>
       <form onSubmit={handleSubmit(onSubmit)} className={css.form}>
         <div className={css.avatarUploadDiv}>
-          <img className={css.avatar} src={avatarUrl} alt="avatar" />
+          <img className={css.avatar} src={preview} alt="avatar" />
           <label className={css.labelUpload}>
             <svg className={css.upload}>
               <use href={`${newSprite}#icon-upload`} />
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -18,7 +18,7 @@ axios.interceptors.response.use(
   async (error) => {
     console.log("Interceptor caught an error", error);
     const { config, response, status } = error;
-    if (status === 401 && response.status === 401 && config._retry) {
+    if (status === 401 && response.status === 401 && !config._retry) {
       config._retry = true;
       try {
         const { store } = await import('../store');
