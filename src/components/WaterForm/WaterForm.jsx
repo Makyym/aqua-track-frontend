fab0f082406d09b885800d2b4fe270f816b8d287
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2025-03-16 16:44:48 +0100

    trying to commit changes

--- src/components/WaterForm/WaterForm.jsx
+++ src/components/WaterForm/WaterForm.jsx
@@ -53,7 +53,6 @@ const WaterForm = ({ value, date }) => {
     }
   };
 
-  // const timeValue = watch('time') || currentTime;
   const waterValue = watch('value') ?? 50;
 
   const handlePlusClick = () => {
