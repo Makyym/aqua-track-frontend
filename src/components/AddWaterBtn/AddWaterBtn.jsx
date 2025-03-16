96682209a725672100e2e599f802e2df32b5f2be
Makyym <168359394+Makyym@users.noreply.github.com>
2025-03-16 18:15:40 +0200

    add water modal logic

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,20 +1,32 @@
+import { useState } from 'react';
 import sprite from '../../assets/newSprite.svg';
 import s from './AddWaterBtn.module.css';
+import BaseModal from '../BaseModal/BaseModal.jsx';
+import WaterModal from '../WaterModal/WaterModal.jsx';
 
-const AddWaterBtn = ({ daily, onOpen }) => {
+const AddWaterBtn = ({ daily }) => {
+  const [waterOpen, SetWaterOpen] = useState(false);
+  const handleModalClose = () => {
+    SetWaterOpen(false);
+  };
   return (
-    <button className={daily ? s.button : s.buttonDaily} onClick={onOpen}>
-      {daily ? (
-        <svg className={s.icon}>
-          <use href={`${sprite}#icon-plus-green`} />
-        </svg>
-      ) : (
-        <svg className={s.iconDaily}>
-          <use href={`${sprite}#icon-plus-white`} />
-        </svg>
-      )}
-      Add water
-    </button>
+    <>
+      <button className={daily ? s.button : s.buttonDaily} onClick={() => SetWaterOpen(true)}>
+        {daily ? (
+          <svg className={s.icon}>
+            <use href={`${sprite}#icon-plus-green`} />
+          </svg>
+        ) : (
+          <svg className={s.iconDaily}>
+            <use href={`${sprite}#icon-plus-white`} />
+          </svg>
+        )}
+        Add water
+      </button>
+      <BaseModal isOpen={waterOpen} onRequestClose={handleModalClose}>
+      <WaterModal />
+      </BaseModal>
+    </>
   );
 };
 
