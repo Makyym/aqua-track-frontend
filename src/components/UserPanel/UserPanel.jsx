768140861a6a7447a7741d50309fc5b55710f5d9
Makyym <168359394+Makyym@users.noreply.github.com>
2025-03-16 17:57:59 +0200

    complete

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -5,10 +5,12 @@ import { selectUser } from "../../redux/auth/selectors.js";
 
 const UserPanel = () => {
     const user = useSelector(selectUser);
+    const {name, email} = user;
+    const shortEmail = email.split('@')[0];
     return (
         <div className={s.div}>
             <h1 className={s.hello}>
-                Hello<span className={s.userName}>, User!</span>
+                Hello<span className={s.userName}>, {name ?? shortEmail}!</span>
             </h1>
             <div><UserBar /></div>
         </div>
